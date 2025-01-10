8e6248f47690925bc8876808ae0831a2b61214ce
Randil <randiltharusha72@gmail.com>
2025-01-10 12:28:08 +0530

    Add copyright headers and improve documentation for password hashing classes

--- native/src/main/java/io/ballerina/stdlib/crypto/nativeimpl/Password.java
+++ native/src/main/java/io/ballerina/stdlib/crypto/nativeimpl/Password.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.ballerina.stdlib.crypto.nativeimpl;
 
 import io.ballerina.runtime.api.utils.StringUtils;
@@ -8,10 +25,23 @@ import org.bouncycastle.crypto.generators.BCrypt;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Native implementation of BCrypt password hashing functions.
+ * Provides methods for hashing passwords, verifying hashes, and generating salts using the BCrypt algorithm.
+ *
+ * @since ...
+ */
 public class Password {
     
     private Password() {}
 
+    /**
+     * Hash a password using BCrypt with a custom work factor.
+     *
+     * @param password the password to hash
+     * @param workFactor the work factor to use (determines computational complexity)
+     * @return hashed password string or error
+     */
     public static Object hashPassword(BString password, long workFactor) {
         try {
             Object validationError = PasswordUtils.validateWorkFactor(workFactor);
@@ -31,10 +61,23 @@ public class Password {
         }
     }
 
+    /**
+     * Hash a password using BCrypt with the default work factor.
+     *
+     * @param password the password to hash
+     * @return hashed password string or error
+     */
     public static Object hashPassword(BString password) {
         return hashPassword(password, PasswordUtils.DEFAULT_WORK_FACTOR);
     }
 
+    /**
+     * Verify a password against a BCrypt hash.
+     *
+     * @param password the password to verify
+     * @param hashedPassword the hashed password to verify against
+     * @return true if password matches, false if not, or error if verification fails
+     */
     public static Object verifyPassword(BString password, BString hashedPassword) {
         try {
             String hash = hashedPassword.getValue();
@@ -61,6 +104,12 @@ public class Password {
         }
     }
 
+    /**
+     * Generate a salt string for BCrypt with a custom work factor.
+     *
+     * @param workFactor the work factor to use
+     * @return formatted salt string or error
+     */
     public static Object generateSalt(long workFactor) {
         try {
             Object validationError = PasswordUtils.validateWorkFactor(workFactor);
@@ -77,6 +126,11 @@ public class Password {
         }
     }
 
+    /**
+     * Generate a salt string for BCrypt with the default work factor.
+     *
+     * @return formatted salt string or error
+     */
     public static Object generateSalt() {
         return generateSalt(PasswordUtils.DEFAULT_WORK_FACTOR);
     }
--- native/src/main/java/io/ballerina/stdlib/crypto/nativeimpl/PasswordArgon2.java
+++ native/src/main/java/io/ballerina/stdlib/crypto/nativeimpl/PasswordArgon2.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.ballerina.stdlib.crypto.nativeimpl;
 
 import io.ballerina.runtime.api.utils.StringUtils;
@@ -10,16 +27,52 @@ import org.bouncycastle.util.encoders.Base64;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Native implementation of Argon2 password hashing functions.
+ * Provides methods for hashing passwords, verifying hashes, and generating salts using the Argon2id algorithm.
+ *
+ * @since ...
+ */
 public class PasswordArgon2 {
+    /**
+     * Default number of iterations for Argon2.
+     */
     private static final int DEFAULT_ITERATIONS = 3;
-    private static final int DEFAULT_MEMORY = 65536;  // 64MB in KB
+    
+    /**
+     * Default memory usage in KB (64MB).
+     */
+    private static final int DEFAULT_MEMORY = 65536;
+    
+    /**
+     * Default number of parallel threads.
+     */
     private static final int DEFAULT_PARALLELISM = 4;
+    
+    /**
+     * Length of the generated hash in bytes.
+     */
     private static final int HASH_LENGTH = 32;
     
+    /**
+     * Hash a password using Argon2 with default parameters.
+     *
+     * @param password the password to hash
+     * @return hashed password string or error
+     */
     public static Object hashPasswordArgon2(BString password) {
         return hashPasswordArgon2(password, DEFAULT_ITERATIONS, DEFAULT_MEMORY, DEFAULT_PARALLELISM);
     }
 
+    /**
+     * Hash a password using Argon2 with custom parameters.
+     *
+     * @param password the password to hash
+     * @param iterations number of iterations
+     * @param memory memory usage in KB
+     * @param parallelism number of parallel threads
+     * @return hashed password string or error
+     */
     public static Object hashPasswordArgon2(BString password, long iterations, long memory, long parallelism) {
         try {
             if (iterations <= 0) {
@@ -57,6 +110,13 @@ public class PasswordArgon2 {
         }
     }
 
+    /**
+     * Verify a password against an Argon2 hash.
+     *
+     * @param password the password to verify
+     * @param hashedPassword the hashed password to verify against
+     * @return true if password matches, false if not, or error if verification fails
+     */
     public static Object verifyPasswordArgon2(BString password, BString hashedPassword) {
         try {
             String hash = hashedPassword.getValue();
@@ -95,10 +155,23 @@ public class PasswordArgon2 {
         }
     }
 
+    /**
+     * Generate a salt string for Argon2 with default parameters.
+     *
+     * @return formatted salt string or error
+     */
     public static Object generateSaltArgon2() {
         return generateSaltArgon2(DEFAULT_ITERATIONS, DEFAULT_MEMORY, DEFAULT_PARALLELISM);
     }
 
+    /**
+     * Generate a salt string for Argon2 with custom parameters.
+     *
+     * @param iterations number of iterations
+     * @param memory memory usage in KB
+     * @param parallelism number of parallel threads
+     * @return formatted salt string or error
+     */
     public static Object generateSaltArgon2(long iterations, long memory, long parallelism) {
         try {
             byte[] salt = PasswordUtils.generateRandomSalt();
